c7c994f0df1a4e7a0b3338c0ab49e37b89224fc3
clerc benjamin <benjamin-clerc@hotmail.fr>
2023-12-14 16:59:20 +0100

    update dashboard

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -28,17 +28,26 @@ $results = $query->fetchAll();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     <title>Administration</title>
 </head>
 <body>
+    <nav class="navbar bg-primary" data-bs-theme="dark">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="dashboard.php">Administration</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link text-danger" href="logout.php">Déconnexion</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
     <div class="container p-3">
-        <h1>Administration</h1>
-        <a href="logout.php">Déconnexion</a>
-
-        <p>Ici afficher un tableau contenant tous les articles avec les données suivants :
-        ID, Titre, nom de categorie ,Date de publication et une colonne "actions"
-
-        La colonne "actions" contiendra 2 liens : Editer et supprimer</p>
 
         <table class="table">
             <thead>
@@ -58,8 +67,11 @@ $results = $query->fetchAll();
                 <th scope="row"><?php echo $data['id']; ?></th>
                 <td><?php echo $data['title']; ?></td>
                 <td><?php echo $data['categories']; ?></td>
-                <td><?php echo $data['publication_date']; ?></td>
-                <td><a href="">Editer</a> | <a href="">Supprimer</a></td>
+                <td><?php echo DateTime::createFromFormat('Y-m-d H:i:s', $data['publication_date'])->format('d-m-Y');?></td>
+                <td>
+                    <a href="#" class="btn btn-light btn-sm">Editer</a>
+                    <a href="#" class="btn btn-danger btn-sm">Supprimer</a>
+                </td>
                 </tr>
                 <?php
                     endforeach;
